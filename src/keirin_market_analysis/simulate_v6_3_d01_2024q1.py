5a2152f6e74d3db6be8c0c40b3b6823dc9cc1f7a
yuhevita-wq <yuhe.vita@gmail.com>
2026-09-01 16:05:06 +0900

    Run v7.7-F08 Q1 simulation

--- src/keirin_market_analysis/simulate_v6_3_d01_2024q1.py
+++ src/keirin_market_analysis/simulate_v6_3_d01_2024q1.py
@@ -36,3 +36,4 @@ if __name__=='__main__':
  from simulate_v7_4_f05_2024q1 import main as x8;x8()
  from simulate_v7_5_f06_2024q1 import main as x9;x9()
  from simulate_v7_6_f07_2024q1 import main as x10;x10()
+ from simulate_v7_7_f08_2024q1 import main as x11;x11()
